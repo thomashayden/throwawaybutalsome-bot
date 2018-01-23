10e73328d637a408e9db2c47eb22c12d34cee697
Thomas Hayden <hayden.t@husky.neu.edu>
2018-01-22 21:58:55 -0500

    Add full system. Now it can be run indefinately (unless it crashes) with one command.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import datetime
 
 import cred
+from reddit import pull_reddit
 
 from markov_chain import markov_chain
 
@@ -16,61 +17,67 @@ reddit = praw.Reddit(client_id=cred.client_id,
 
 neu = reddit.subreddit("NEU")
 
-try:
-    with open("data.pkl", "rb") as f:
-        data = pickle.load(f)
-        print("Loaded markov data successfully")
-except (OSError, IOError) as e:
-    print("Unable to markov data file. Run reddit.py before running this.")
-    sys.exit()
+while True:
+    print("Starting search loop")
+    rd = pull_reddit()
 
-better_data = []
-for dat in data:
-    better_data.append(dat.split())
-mc = markov_chain(better_data)
-
-for posts in neu.new(limit=10):
-    should_post = False;
-    for comment in posts.comments:
-        if comment.author == "throwawaybutalsome":
-            should_post = True
-    if not should_post:
-        continue
     try:
-        with open("posted.pkl", "rb") as f:
-            posted = pickle.load(f)
-            print("Loaded posted.pkl successfully!")
+        with open("data.pkl", "rb") as f:
+            data = pickle.load(f)
+            print("Loaded markov data successfully")
     except (OSError, IOError) as e:
-        print("Unable to load posted.pkl. Creating file.")
-        posted = []
-        with open("posted.pkl", "wb") as f:
-            pickle.dump(posted, f)
+        print("Unable to markov data file. Try manually running reddit.py before running this.")
+        sys.exit()
 
-    url = "https://reddit.com" + posts.permalink
+    better_data = []
+    for dat in data:
+        better_data.append(dat.split())
+    mc = markov_chain(better_data)
 
-    if url not in posted:
-        line = mc.generate_line()
+    for posts in neu.new(limit=10):
+        should_post = False;
+        for comment in posts.comments:
+            if comment.author == "throwawaybutalsome":
+                should_post = True
+        if not should_post:
+            continue
+        try:
+            with open("posted.pkl", "rb") as f:
+                posted = pickle.load(f)
+                print("Loaded posted.pkl successfully!")
+        except (OSError, IOError) as e:
+            print("Unable to load posted.pkl. Creating file.")
+            posted = []
+            with open("posted.pkl", "wb") as f:
+                pickle.dump(posted, f)
 
-        line = mc.generate_line()
-        print(line)
+        url = "https://reddit.com" + posts.permalink
 
-        post_body = line + """
-***
-*^Beep ^Boop. ^I'm ^a ^bot ^that ^uses ^markov ^chains ^to ^imitate ^the ^annoying ^comments ^of ^throwawaybutalsome.*
-*^Unless ^people ^actually ^like ^me^for^some^reason ^, ^I ^will ^only ^comment ^on ^a ^few ^posts ^before ^disappearing ^forever.*"""
+        if url not in posted:
+            line = mc.generate_line()
+
+            line = mc.generate_line()
+            print(line)
+
+            post_body = line + """
+    ***
+    *^Beep ^Boop. ^I'm ^a ^bot ^that ^uses ^markov ^chains ^to ^imitate ^the ^annoying ^comments ^of ^throwawaybutalsome.*
+    *^Unless ^people ^actually ^like ^me^for^some^reason ^, ^I ^will ^only ^comment ^on ^a ^few ^posts ^before ^disappearing ^forever.*"""
 
 
 
 
-        print("Replying")
-        posts.reply(post_body) # API Call
-        print("Replied with: " + post_body)
-        print("Time is: " + str(datetime.datetime.hour) + ":" + str(datetime.datetime.minute) + ":" + str(datetime.datetime.second))
-        print("Sleeping for 12 minutes")
-        posted.append(url)
-        with open('posted.pkl', 'wb') as f:
-            pickle.dump(posted, f)
-            print("Saved data")
-        print("Sleeping")
-        time.sleep(60 * 12)
-    print("Finished analyzing " + url)
+            print("Replying")
+            posts.reply(post_body) # API Call
+            print("Replied with: " + post_body)
+            print("Time is: " + str(datetime.datetime.now()))
+            print("Sleeping for 12 minutes")
+            posted.append(url)
+            with open('posted.pkl', 'wb') as f:
+                pickle.dump(posted, f)
+                print("Saved data")
+            print("Sleeping")
+            time.sleep(60 * 12)
+        print("Finished analyzing " + url)
+    print("End of search loop. Sleeping for a while. Current time: " + str(datetime.datetime.now()) + ".\n Resume time should be: " + str(datetime.datetime.now() + datetime.timedelta(minutes=30)))
+    time.sleep(60*30) # Wait 30 minutes and then check for more posts.
--- reddit.py
+++ reddit.py
@@ -4,59 +4,61 @@ import time
 
 import cred
 
-reddit = praw.Reddit(client_id=cred.client_id,
-                     client_secret=cred.secret,
-                     username=cred.username,
-                     password=cred.password,
-                     user_agent=cred.user_agent)
+class pull_reddit:
+    def updateRedditFile(self):
+        reddit = praw.Reddit(client_id=cred.client_id,
+                             client_secret=cred.secret,
+                             username=cred.username,
+                             password=cred.password,
+                             user_agent=cred.user_agent)
 
-neu = reddit.subreddit("NEU")
+        neu = reddit.subreddit("NEU")
 
-try:
-    with open("data.pkl", "rb") as f:
-        data = pickle.load(f)
-    print("Loaded file correctly")
-except (OSError, IOError) as e:
-    data = []
-    with open("data.pkl", "wb") as f:
-        pickle.dump(data, f)
-    print("Unable to load file. Created new one.")
+        try:
+            with open("data.pkl", "rb") as f:
+                data = pickle.load(f)
+            print("Loaded file correctly")
+        except (OSError, IOError) as e:
+            data = []
+            with open("data.pkl", "wb") as f:
+                pickle.dump(data, f)
+            print("Unable to load file. Created new one.")
 
-user_based_collection = True
+        user_based_collection = True
 
-if user_based_collection:
-    thrbutal = reddit.redditor("throwawaybutalsome")
-    for comment in thrbutal.comments.new():
-        if comment.subreddit == "neu":
-            for line in comment.body.split("\n"):
-                if not line == "" and not line[0].isdigit(): # Check that the line is not empty and doesn't start with a number (there are lines which start with a number that are useless and mess up the data)
-                    if not data.__contains__(line):
-                        data.append(line)
-                        print(line)
-                    else:
-                        print("========== Found Duplicate")
+        if user_based_collection:
+            thrbutal = reddit.redditor("throwawaybutalsome")
+            for comment in thrbutal.comments.new():
+                if comment.subreddit == "neu":
+                    for line in comment.body.split("\n"):
+                        if not line == "" and not line[0].isdigit(): # Check that the line is not empty and doesn't start with a number (there are lines which start with a number that are useless and mess up the data)
+                            if not data.__contains__(line):
+                                data.append(line)
+                                print(line)
+                            else:
+                                print("========== Found Duplicate")
+                else:
+                    print("========== In subreddit: " + comment.subreddit.title)
         else:
-            print("========== In subreddit: " + comment.subreddit.title)
-else:
-    start_time = time.time()
+            start_time = time.time()
 
-    new_neu = neu.new()
+            new_neu = neu.new()
 
-    for submission in new_neu:
-        #print(submission.title)
-        for comment in submission.comments:
-            if comment.author == "throwawaybutalsome":
-                for line in comment.body.split("\n"):
-                    if not line == "":
-                        if not data.__contains__(line):
-                            data.append(line)
-                            print("Added " + line)
-                        else:
-                            print("Found duplicate")
-        if time.time() - start_time > 60 * 10: # Run for 10 minutes, then break
-            break
-        time.sleep(2)  # The API is limited to 30 every minute, so there is a 2 second wait between each call
+            for submission in new_neu:
+                #print(submission.title)
+                for comment in submission.comments:
+                    if comment.author == "throwawaybutalsome":
+                        for line in comment.body.split("\n"):
+                            if not line == "":
+                                if not data.__contains__(line):
+                                    data.append(line)
+                                    print("Added " + line)
+                                else:
+                                    print("Found duplicate")
+                if time.time() - start_time > 60 * 10: # Run for 10 minutes, then break
+                    break
+                time.sleep(2)  # The API is limited to 30 every minute, so there is a 2 second wait between each call
 
-with open('data.pkl', 'wb') as f:
-    pickle.dump(data, f)
-    print("Saved data")
+        with open('data.pkl', 'wb') as f:
+            pickle.dump(data, f)
+            print("Saved data")
